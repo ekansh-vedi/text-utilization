2834d7264a525256e879f922e8d610de70e76d63
EKANSH VEDI <vediekansh@gmail.com>
2024-03-29 14:11:52 +0530

    removed unused dependencies

--- src/Dictionary.jsx
+++ src/Dictionary.jsx
@@ -1,12 +1,12 @@
-import   { useState } from "react";
+import { useState } from "react";
 import Axios from "axios";
-import { FaSearch } from "react-icons/fa";
-import { FcSpeaker } from "react-icons/fc";
+import { FaSearch, FaCopy } from "react-icons/fa";
+ 
 
 function App() {
   const [data, setData] = useState("");
   const [searchWord, setSearchWord] = useState("");
-
+  
   const getMeaning = async () => {
     try {
       const response = await Axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en_US/${searchWord}`);
@@ -16,9 +16,15 @@ function App() {
     }
   };
 
-  const playAudio = () => {
-    let audio = new Audio(data.phonetics[0].audio);
-    audio.play();
+  const copyToClipboard = (text) => {
+    navigator.clipboard.writeText(text)
+      .then(() => {
+        console.log("Copied to clipboard:", text);
+        alert("Copied to clipboard!");
+      })
+      .catch((error) => {
+        console.error("Error copying to clipboard:", error);
+      });
   };
 
   return (
@@ -39,14 +45,19 @@ function App() {
         <div className="showResults">
           <h2>
             {data.word}{" "}
-            <button onClick={playAudio}>
-              <FcSpeaker size="26px" />
+            <button onClick={() => copyToClipboard(data.word)}>
+              <FaCopy size="20px" />
             </button>
           </h2>
           <h4>Parts of speech:</h4>
           <p>{data.meanings[0].partOfSpeech}</p>
           <h4>Definition:</h4>
-          <p>{data.meanings[0].definitions[0].definition}</p>
+          <p>
+            {data.meanings[0].definitions[0].definition}{" "}
+            <button onClick={() => copyToClipboard(data.meanings[0].definitions[0].definition)}>
+              <FaCopy size="20px" />
+            </button>
+          </p>
         </div>
       )}
     </div>
